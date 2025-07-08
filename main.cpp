5051482a23f3a8a116dc317aaaecc3dff7786b4e
Ryan James <realcoder2023@gmail.com>
2025-07-08 13:24:34 -0400

    added initialized project code

---
+++ main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+int main() {
+    std::string fileName;
+    std::cin >> fileName;
+    std::string cloneURL;
+
+    std::cout << "Enter URL: " << "\n";
+    std::cin >> cloneURL;
+
+    if (std::ofstream file(fileName); file.is_open()) {
+
+        file << "git clone " << cloneURL;
+        file << "git add . \n";
+        file << "git commit -m 'Added Code' \n";
+        file << "git push \n";
+
+
+        file.close();
+
+
+    }
+
+}
+
